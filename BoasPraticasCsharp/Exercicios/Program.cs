f3323133b789ec7a095f40d592bcb6222b6a5bc5
Gustavo Vieira <gustavieiradev@gmail.com>
2025-04-25 06:23:25 -0300

    feat: Done the first exercise to create a program to calculate basic operations.

--- BoasPraticasCsharp/Exercicios/Program.cs
+++ BoasPraticasCsharp/Exercicios/Program.cs
@@ -1,10 +1,70 @@
-﻿namespace Exercicios
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Exercicios
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int numero1 = LerNumero();
+            int numero2 = LerNumero();
+
+            int soma = SomarNumeros(numero1, numero2);
+            Console.WriteLine($"A soma é: {soma}");
+
+            int subtracao = SubtrairNumeros(numero1, numero2);
+            Console.WriteLine($"A subtração é: {subtracao}");
+
+            int multiplicacao = MultiplicarNumeros(numero1, numero2);
+            Console.WriteLine($"A multiplicação é: {multiplicacao}");
+
+            double divisao = DividirNumeros(numero1, numero2);
+            if (divisao != 0)
+            {
+                Console.WriteLine($"A divisão é: {divisao}");
+            }
+            else
+            {
+                Console.WriteLine($"Não é possível dividir por zero.");
+            }
+
+            Console.ReadLine();
+
+        }
+
+
+        static int LerNumero()
+        {
+            Console.WriteLine("Digite um número: ");
+            int numero = int.Parse(Console.ReadLine());
+            return numero;
+        }
+
+        static int SomarNumeros(int numero1, int numero2)
+        {
+            return numero1 + numero2;
+        }
+
+        static int SubtrairNumeros(int numero1, int numero2)
+        {
+            return numero1 - numero2;
+        }
+
+        static int MultiplicarNumeros(int numero1, int numero2)
+        {
+            return numero1 * numero2;
+        }
+
+        static double DividirNumeros(int numero1, int numero2)
+        {
+            if (numero2 != 0)
+            {
+                return (double)numero1 / numero2;
+            }
+            else
+            {
+                return 0;
+            }
         }
     }
 }
