6321d7b77b6d385283c90bf7ef6728b893bf8a18
Gustavo Vieira <gustavieiradev@gmail.com>
2025-04-26 07:57:14 -0300

    Applied system commands separately to apply the concept of Design Patterns.

--- BoasPraticasCsharp/Alura.Adopet.Console/Program.cs
+++ BoasPraticasCsharp/Alura.Adopet.Console/Program.cs
@@ -1,52 +1,21 @@
-﻿using System.Net.Http.Headers;
-using Alura.Adopet.Console.Comandos;
+﻿using Alura.Adopet.Console.Comandos;
 
-// na linha abaixo cria-se uma instância de HttpClient para consumir API Adopet.
-HttpClient client = ConfiguraHttpClient("http://localhost:5057");
+ComandosDoSistema comandos = new();
+
 Console.ForegroundColor = ConsoleColor.Green;
 try
 {
     string comando = args[0].Trim();
-    switch (comando)
-    {
-        case "import":
-            var import = new Import();
-            await import.ExecutarAsync(args);
-            break;
-        case "help":
-            var help = new Help();
-            await help.ExecutarAsync(args);
-            break;
-        case "show":
-            var show = new Show();
-            await show.ExecutarAsync(args);
-            break;
-        case "list":
-            var list = new List();
-            await list.ExecutarAsync(args);
-            break;
-        default:
-            Console.WriteLine("Comando inválido!");
-            break;
-    }
+    IComando? cmd = comandos[comando];
+    if (cmd is not null) await cmd.ExecutarAsync(args);
+    else Console.WriteLine("Comando inválido!");
 }
 catch (Exception ex)
 {
-    // mostra a exceção em vermelho
     Console.ForegroundColor = ConsoleColor.Red;
     Console.WriteLine($"Aconteceu um exceção: {ex.Message}");
 }
 finally
 {
     Console.ForegroundColor = ConsoleColor.White;
-}
-
-HttpClient ConfiguraHttpClient(string url)
-{
-    HttpClient _client = new HttpClient();
-    _client.DefaultRequestHeaders.Accept.Clear();
-    _client.DefaultRequestHeaders.Accept.Add(
-        new MediaTypeWithQualityHeaderValue("application/json"));
-    _client.BaseAddress = new Uri(url);
-    return _client;
 }
